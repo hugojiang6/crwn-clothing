17bfd3080648a00bb28d959a9cbd5f7574f23872
hugojiang6 <wade@ms2.url.com.tw>
2023-08-14 14:25:06 +0800

    updating our directory page with directory component and category item component

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Directory from './component/directory/directory.component.jsx';
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: '帽子',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: '夾克',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: '運動鞋',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: '女性',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: '男性',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
+
+export default App;
