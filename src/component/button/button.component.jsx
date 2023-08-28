e544f59a6ff1f63c3e7d6b5b4df5f6716fb058bf
hugojiang6 <wade@ms2.url.com.tw>
2023-08-28 14:53:09 +0800

    added in our addtocart feature

--- src/component/button/button.component.jsx
+++ src/component/button/button.component.jsx
@@ -6,7 +6,7 @@ const BUTTON_TYPE_CLASSES = {
 };
 
 const Button = ({ children, buttonType, buttonOptions }) => {
-
+  
   return (
     <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} {...buttonOptions}>
       {children}
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,46 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+
+// 加入購物車Add to cart按鈕
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
 
-  return <CartContext.Provider value={{ isCartOpen, setIsCartOpen }}>{children}</CartContext.Provider>;
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total, cartItem) => {
+      return total + cartItem.quantity;
+    }, 0);
+
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  return (
+    <CartContext.Provider value={{ isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
